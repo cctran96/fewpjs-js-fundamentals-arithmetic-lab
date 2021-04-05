a63c07b72deb590efc71ac98232d91b804242753
Chau Tran <cctran96@gmail.com>
2021-04-05 13:59:40 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 62
+let num2 = 1
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 31
+let num4 = 9
+const mod = num3 % num4
+
+const max = Math.max(1,2,20)
